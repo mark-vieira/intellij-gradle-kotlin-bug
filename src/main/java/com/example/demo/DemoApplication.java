2d39cd0eb493f926fa70f2c57a550904cc15c5b9
Mark Vieira <portugee@gmail.com>
2020-05-20 15:28:59 -0700

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		DataBuffer buffer = null;
+        DataBufferUtils.join(Flux.just(buffer));
+	}
+
+}
